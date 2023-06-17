76e81fc61f6e9fc8c09086fc70a9eb01e0ebcfda
Conrad <conrad.rzuc@gmail.com>
2023-06-17 23:49:08 +0100

    Update NoAdsYt.js
    Optimization changes.
    
    "let ogVolume=1;
    let pbRate = 1; " removed, not necessary.
    
    "                if(document.getElementsByClassName("ytp-ad-text ytp-ad-preview-text")[0]!==undefined){
                        vid.playbackRate=16;"
    
    it can be removed just normally.
    
    Instead of calling the function every 100ms, the DOM change listener added
    
    "// Create a MutationObserver to detect changes in the DOM
    const observer = new MutationObserver((mutationsList) => {
      for (const mutation of mutationsList) {
        if (mutation.type === "childList" || mutation.type === "subtree") {
          if (document.querySelector(".video-stream.html5-main-video") !== null) {
            removeAds();
          }
        }
      }
    });
    
    // Start observing changes in the DOM
    observer.observe(document.documentElement, { childList: true, subtree: true });"
    
    functions changed to const arrow function, to let know the interpreter they won't be changed/modified/reassigned - for optimization.
    
    let changed to const; similar logic as above
    
    Use forEach loop instead of for loop - optimization
    
    Consolidate selector checks:
    Combine the checks for sideAd and sideAd_ into one selector.
    Combine the checks for skipBtn and incomingAd into one selector.

--- NoAdsYt.js
+++ NoAdsYt.js
@@ -1,61 +1,67 @@
 // ==UserScript==
 // @name         No ADS - YouTube
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  - Skips all youtube ads - | - undetectable - | - skips ads instantly -
-// @author       GSRHackZ
+// @author       GSRHackZ | optimization changes - ConradRz
 // @match        https://www.youtube.com/*
 // @match        https://m.youtube.com/*
 // @icon         https://i.ibb.co/X5f50Cg/Screen-Shot-2021-07-19-at-9-31-54-PM.png
 // @grant        none
 // ==/UserScript==
 
-let ogVolume=1;
-let pbRate = 1;
+const removeAds = () => {
+  const ad = document.querySelector(".video-ads.ytp-ad-module");
 
-setInterval(function(){
-    if(document.getElementsByClassName("video-stream html5-main-video")[0]!==undefined){
-        let ad = document.getElementsByClassName("video-ads ytp-ad-module")[0];
-        let vid = document.getElementsByClassName("video-stream html5-main-video")[0];
-        if(ad==undefined){
-            pbRate = vid.playbackRate;
-        }
-        let closeAble = document.getElementsByClassName("ytp-ad-overlay-close-button");
-        for(let i=0;i<closeAble.length;i++){
-            closeAble[i].click();
-            //console.log("ad banner closed!")
-        }
-        if(document.getElementsByClassName("style-scope ytd-watch-next-secondary-results-renderer sparkles-light-cta GoogleActiveViewElement")[0]!==undefined){
-            let sideAd=document.getElementsByClassName("style-scope ytd-watch-next-secondary-results-renderer sparkles-light-cta GoogleActiveViewElement")[0];
-            sideAd.style.display="none";
-            //console.log("side ad removed!")
-        }
-        if(document.getElementsByClassName("style-scope ytd-item-section-renderer sparkles-light-cta")[0]!==undefined){
-            let sideAd_ = document.getElementsByClassName("style-scope ytd-item-section-renderer sparkles-light-cta")[0];
-            sideAd_.style.display="none";
-            //console.log("side ad removed!")
-        }
-        if(document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text")[0]!==undefined){
-            let skipBtn=document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text")[0];
-            skipBtn.click();
-            //console.log("skippable ad skipped!")
-        }
-        if(document.getElementsByClassName("ytp-ad-message-container")[0]!==undefined){
-            let incomingAd=document.getElementsByClassName("ytp-ad-message-container")[0];
-            incomingAd.style.display="none";
-            //console.log("removed incoming ad alert!")
-        }
-        if(document.getElementsByClassName("style-scope ytd-companion-slot-renderer")[0]!==undefined){
-            document.getElementsByClassName("style-scope ytd-companion-slot-renderer")[0].remove();
-            //console.log("side ad removed!")
-        }
-        if(ad!==undefined){
-            if(ad.children.length>0){
-                if(document.getElementsByClassName("ytp-ad-text ytp-ad-preview-text")[0]!==undefined){
-                    vid.playbackRate=16;
-                    //console.log("Incrementally skipped unskippable ad!")
-                }
-            }
-        }
+  const closeButtons = document.querySelectorAll(".ytp-ad-overlay-close-button");
+  closeButtons.forEach((button) => {
+    button.click();
+    // console.log("ad banner closed!");
+  });
+
+  const sideAds = document.querySelectorAll(".style-scope.ytd-watch-next-secondary-results-renderer.sparkles-light-cta.GoogleActiveViewElement, .style-scope.ytd-item-section-renderer.sparkles-light-cta");
+  sideAds.forEach((sideAd) => {
+    sideAd.style.display = "none";
+    // console.log(".style-scope.ytd-watch-next-secondary-results-renderer.sparkles-light-cta.GoogleActiveViewElement, .style-scope.ytd-item-section-renderer.sparkles-light-cta ad removed!");
+  });
+
+  const skipBtn = document.querySelector(".ytp-ad-text.ytp-ad-skip-button-text");
+  if (skipBtn !== null) {
+    skipBtn.click();
+    // console.log("skippable ad skipped!");
+  }
+
+  const incomingAd = document.querySelector(".ytp-ad-message-container");
+  if (incomingAd !== null) {
+    incomingAd.style.display = "none";
+    // console.log("removed incoming ad alert!");
+  }
+
+  const companionSlot = document.querySelector(".style-scope.ytd-companion-slot-renderer");
+  if (companionSlot !== null) {
+    companionSlot.parentNode.removeChild(companionSlot);
+    // console.log(".style-scope.ytd-companion-slot-renderer ad removed!");
+  }
+
+  if (ad !== null && ad.children.length > 0) {
+    const previewText = document.querySelector(".ytp-ad-text.ytp-ad-preview-text");
+    if (previewText !== null) {
+      previewText.parentNode.removeChild(previewText);
+      // console.log("unskippable ad removed!");
     }
-},100)
+  }
+};
+
+// Create a MutationObserver to detect changes in the DOM
+const observer = new MutationObserver((mutationsList) => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList" || mutation.type === "subtree") {
+      if (document.querySelector(".video-stream.html5-main-video") !== null) {
+        removeAds();
+      }
+    }
+  }
+});
+
+// Start observing changes in the DOM
+observer.observe(document.documentElement, { childList: true, subtree: true });
